bae17097c805a1b51742270b60c6a2c0805995ac
Vishwas Puri <vishwaspuriofficial@gmail.com>
2023-04-11 13:35:15 -0400

    updated

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from Page import home
 from Page import main
+from Page import write
 
 # Custom imports
 from pages import MultiPage
@@ -14,5 +15,6 @@ st.title("Multi-page Application")
 # Add all your applications (pages) here
 app.add_page("Home", home.app)
 app.add_page("Main", main.app)
+app.add_page("Write", write.app)
 # The main app
 app.run()
